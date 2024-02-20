5e032c2f569e45115aa94155830cae02f92a6c5b
resbaa <sadobk4@gmail.com>
2024-02-20 16:33:06 +0300

    20.02.2024

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,8 @@
+﻿using GenericsIntro;
+using System.Globalization;
+
+static void Main(string[] args)
+{
+    MyList<string> isimler = new MyList<string>();
+    isimler.Add("Sadettin");
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,17 @@
+﻿using OOP1;
+
+Product product1 = new Product();
+product1.Id = 1;
+product1.CategoryID = 2;
+product1.ProductName = "Masa";
+product1.UnitPrice = 500;
+product1.UnitsInStock = 3;
+
+
+Product product2 = new Product { Id = 2, CategoryID = 5, 
+    UnitPrice = 35, ProductName = "Kalem", UnitsInStock = 5 };
+
+ProductManager productManager = new ProductManager();
+productManager.Add(product1);   
+Console.WriteLine(product1.ProductName);
+
