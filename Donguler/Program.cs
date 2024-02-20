2c6026b1f2d0c7f10cca2067588108e4521b51ce
smurf <smurf@DESKTOP-RLJFN8K>
2024-02-20 09:58:56 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,23 @@
+﻿string kurs1 = "Yazılım Geliştirici Yerleştirme Kampı";
+string kurs2 = "Programlamaya başlangıç için temel kurs";
+string kurs3 = "Java";
+
+//Array - dizi
+
+string[] kurslar = new string[] { "Yazılım Geliştirici Yerleştirme Kampı",
+    "Programlamaya başlangıç için temel kurs",
+    "Java", "Python", "C#" };
+
+
+for (int i = 0; i<kurslar.Length; i++) 
+    
+{
+    Console.WriteLine(kurslar[i]);
+}
+Console.WriteLine("For bitti");
+foreach (string kurs in kurslar)
+{
+    Console.WriteLine(kurs);  
+}
+
+Console.WriteLine("sayfa sonu  - footer");
---
+++ Kampintro/Program.cs
@@ -0,0 +1,34 @@
+﻿
+//type safety - tip güvenliği
+string kategoriEtiketi = "Kategoriler";
+int ogrenciSayisi = 32000;
+double faizOrani = 1.45;
+bool sistemeGirisYapmisMi = true;
+double dolarDun = 7.45;
+double dolarBugun = 7.45;
+
+if (dolarDun>dolarBugun)
+{
+    Console.WriteLine("Azalış butonu");
+}
+else if (dolarDun < dolarBugun)
+{
+    Console.WriteLine("Artış butonu");
+}
+else
+{
+    Console.WriteLine("Değişmedi butonu");
+}
+
+if (sistemeGirisYapmisMi == true)
+{
+    Console.WriteLine("Kullanıcı Ayarları butonu");
+}
+else
+{
+    Console.WriteLine("Giriş yap butonu");
+}
+
+Console.WriteLine(kategoriEtiketi);
+
+
